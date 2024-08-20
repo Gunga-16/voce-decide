47654850daa3a99e6f3935fbba090c6da1226959
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-08-20 09:41:27 -0300

    k

--- script.js
+++ script.js
@@ -9,60 +9,63 @@ const perguntas = [
     {
         enunciado: 'No âmbito social, você prefere:',
         alternativas: [
-        {
-             texto: 'Destruir qualquer forma de exploração humana.',
-             afirmacao: 'Você será escravo até morrer.',
+            {
+                texto: 'Destruir qualquer forma de exploração humana.',
+                afirmacao: 'Você será escravo até morrer.',
 
-        },
+            },
 
-        {
-            texto: 'Acabar com todas as drogas do mundo.',
-            afirmacao: 'Os usuários de drogas morreram por sua causa.',
+            {
+                texto: 'Acabar com todas as drogas do mundo.',
+                afirmacao: 'Os usuários de drogas morreram por sua causa.',
 
-       }
-    ]
+            }
+        ]
     },
-    
+
     {
         enunciado: 'No âmbito ambiental, você prefere:',
         alternativas: [
-        {
-             texto: 'Extinguir a Floresta Amazônica.',
-             afirmacao: 'Os problemas ambientais desapareceram por sua decisão.',
+            {
+                texto: 'Extinguir a Floresta Amazônica.',
+                afirmacao: 'Os problemas ambientais desapareceram por sua decisão.',
 
-        },
+            },
 
-        {
-            texto: 'Acabar com a água do mundo.',
-            afirmacao: 'Parabéns, o ser humano não terá sede.',
+            {
+                texto: 'Acabar com a água do mundo.',
+                afirmacao: 'Parabéns, o ser humano não terá sede.',
 
-       }
-    ]
+            }
+        ]
     },
 
     {
         enunciado: 'No âmbito tecnológico, você prefere:',
         alternativas: [
-        {
-             texto: 'Ter internet para sempre.',
-             afirmacao: 'Parabéns, ninguém pode se comunicar nas redes sociais ou qualquer meio de comunicação digital.',
+            {
+                texto: 'Ter internet para sempre.',
+                afirmacao: 'Parabéns, ninguém pode se comunicar nas redes sociais ou qualquer meio de comunicação digital.',
 
-        },
+            },
 
-        {
-            texto: 'Criar a cura do Câncer.',
-            afirmacao: 'Sua decisão causou que todos os habitantes do mundo terão tumores com sequelas visuais.',
+            {
+                texto: 'Criar a cura do Câncer.',
+                afirmacao: 'Parabéns, todos os habitantes do mundo terão tumores com sequelas visuais.',
 
-       }
-    ]
+            }
+        ]
     }
 
 ]
 
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostraPerguntas () {
+
+function mostraPerguntas() {
     perguntaAtual = perguntas[atual];
 
     caixaPerguntas.textContent = perguntaAtual.enunciado;
@@ -74,18 +77,21 @@ function mostraPerguntas () {
 
 
 function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas){
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function(){
-            atual++;
-            mostraPerguntas();
-        })
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
+
+function respostaSelecionada(alternativa) {
+    const afirmacoes = alternativa.afirmacao;
+    atual++;
+    mostraPerguntas();
+}
+
+
 mostraPerguntas();
-
-
 
